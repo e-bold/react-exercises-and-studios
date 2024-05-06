950cd6899823bdd5e69ea11bbd87a0286281f115
e-bold <enkhmunkh@att.net>
2024-05-06 10:37:45 -0500

    Completed Chapter30 Exercise

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,35 @@
 import './styles.css';
 import Button from './Button';
+import Oceans from './oceans.json'
 
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+      {listItem}
+      </>
    );
-}
+};
+
+let listItem = Oceans.map(Oceans => 
+   <div let correct = "profile" key={Oceans.id} className={`${Oceans.fishCheck === "true" ? "isAFish" : "profile"
+   }`}
+   >
+      <img src={Oceans.image} alt={Oceans.name} 
+      className='img'/>
+      <h1>{Oceans.name}</h1>
+      <h3>Fun Facts: </h3>
+         <ol>
+            <li>{Oceans.fact1}</li>
+            <li>{Oceans.fact2}</li>
+            <li>{Oceans.fact3}</li>
+         </ol>
+   <Button />
+   </div>
+);
+
+
+
 
 export default Profile;
